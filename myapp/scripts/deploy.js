f6ae14bc54fbcedd04272e403b0d161d6647ac49
BruteChouette81 <hbaril1@icloud.com>
2022-02-27 18:04:57 -0500

    hardhat

--- myapp/scripts/deploy.js
+++ myapp/scripts/deploy.js
@@ -1,5 +1,8 @@
+const fs = require('fs');
+const Web3 = require('web3')
 
-
+const web3 = new Web3(Web3.givenProvider || "ws://localhost:8545");
+//run an ethereum node on localhost try to connect to one 
 
 // test deploy
 (async () => {
@@ -11,20 +14,28 @@
     
         // Note that the script needs the ABI which is generated from the compilation artifact.
         // Make sure contract is compiled and artifacts are generated
-        const artifactsPath = `../bin/${contractName}.json` // Change this for different path
+        fs.readFile(`../bin/${contractName}.json`, 'utf8', (err, data) => {
 
-        const metadata = JSON.parse(artifactsPath)
+            if (err) {
+                console.log(`Error reading file from disk: ${err}`);
+            } else {
+
+                // parse JSON string to JSON object
+                var metadata = JSON.parse(data);
+            }
+        });
+
         //get current browser account
         const accounts = await web3.eth.getAccounts()
-        console.log(accounts[0])
-    
-        let contract = new web3.eth.Contract(metadata.abi)
-    
+        console.log("account" + accounts[0]) //my eth account 
+            
+        let contract = new web3.eth.Contract(metadata.abi)//  const contractInstance = web3.eth.contract(abi).at(contractAddress);
+            
         contract = contract.deploy({
             data: metadata.data.bytecode.object,
             arguments: constructorArgs   
         })
-    
+            
         const newContractInstance = await contract.send({
             from: accounts[0],
             gas: 1500000,
@@ -42,7 +53,9 @@
             }
         });
 
+
     } catch (e) {
         console.log(e.message)
+        console.log("error message")
     }
   })()
