6351b303bb6ac07b791c9760811de0bf8f151ea9
brutechouette81 <hbaril1@icloud.com>
2023-03-21 21:25:45 -0400

    New ideas to make the  a more reliable currency

---
+++ client/dip/dipA/iet.sol
@@ -0,0 +1,5 @@
+// SPDX-License-Identifier: GPL-3.0
+
+pragma solidity >=0.7.0 <0.9.0;
+
+// stands for imperial exchange token. used to comfirm smart contract and reduce the crypto repercutions. Build a normal coin and an oracle that is use to algorithmicly back the token 1:1 (special AMM)
